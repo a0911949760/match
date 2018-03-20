e19e376e0f532acb9d7c588a154d9dd166a7650a
阿銜 <a0911949761@gmail.com>
2018-03-20 10:10:32 +0800

    add fields

--- database/migrations/2018_03_14_090120_create_userscare_table.php
+++ database/migrations/2018_03_14_090120_create_userscare_table.php
@@ -16,6 +16,12 @@ class CreateUserscareTable extends Migration
         Schema::create('userscare', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('twzipcode');
+            $table->string('birthday');
+            $table->string('phone');
+            $table->string('speciality');
+            $table->string('experience');
+            $table->string('identitycard');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
