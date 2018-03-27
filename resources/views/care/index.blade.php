99aff44a188a62a599ee58fefc874ea3c9ea669a
阿銜 <a0911949761@gmail.com>
2018-03-27 10:09:37 +0800

    修改被看護列表

--- resources/views/care/index.blade.php
+++ resources/views/care/index.blade.php
@@ -5,7 +5,7 @@
      <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>CRUD</h2>
+                <h2>被看護列表</h2>
             </div>
             <div class="pull-right">
                 <a class="btn btn-success" href="{{ route('care.create') }}"> Create New Article</a>
@@ -27,28 +27,21 @@
             <th>姓名</th>
             <th>E-Mail</th>
             <th>縣市</th>
-            <th>鄉鎮</th>
-            <th>郵遞區號</th>
+            <th>鄉鎮</th>            
             <th>出生年月日</th>
-            <th>連絡電話</th>
-            <th>專長</th>
-            <th>學歷</th>
-            <th>身份證字號</th>            
+            <th>連絡電話</th>                        
             <th width="280px">Action</th>
         </tr>
     @foreach ($articles as $article)
     <tr>
         <td>{{ ++$i }}</td>
         <td>{{ $article->name}}</td>
-        <td>{{ $article->twzipcode}}</td>
-        <td>{{ $article->twzipcode2}}</td>
-        <td>{{ $article->twzipcode3}}</td>
-        <td>{{ $article->birthday}}</td>
-        <td>{{ $article->phone}}</td>
-        <td>{{ $article->speciality}}</td>
-        <td>{{ $article->experience}}</td>
-        <td>{{ $article->identitycard}}</td>
         <td>{{ $article->email}}</td>
+        <td>{{ $article->twzipcode}}</td>
+        <td>{{ $article->twzipcode2}}</td>        
+        <td>{{ $article->birthday}}</td>
+        <td>{{ $article->phone}}</td>        
+        
 
         <td>
             <a class="btn btn-info" href="{{ route('care.show',$article->id) }}">Show</a>
